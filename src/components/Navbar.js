9528de8156a67a68e00c713d8ae1fcc6c077ff83
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-27 17:54:47 +0200

    Adding navbar component basics

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../styles/navbar.css';
+// import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar--wrapper">
+      {/* <Link to='/'>Home</Link>
+      <Link to='shop'>fgh</Link> */}
+      <h2>Shopper</h2>
+
+    </nav>
+  );
+};
+
+export default Navbar;
