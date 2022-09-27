37c41a2f96c0e89007cd092f55461af5cef1daa4
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-27 18:23:40 +0200

    Homepage done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
       <Navbar />
+      {/* <Homepage /> */}
+
       <Routes>
-        
+        <Route path='/' element={<Homepage />}/>
       </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import '../styles/homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className="homepage--wrapper">
+      <span>
+        <p>This homepage was created only to practice react-router-dom,</p>
+        <p>it has no other functionality.</p>
+      </span>
+    </div>
+  );
+};
+
+export default Homepage;
